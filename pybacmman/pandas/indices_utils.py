2b1f1f9f091e9c496cdbbe7aad6e474b6f7a9fed
jean.ollion <jean.ollion@polytechnique.org>
2020-02-13 18:38:11 +0100

    added indices manipulation functions

--- pybacmman/pandas/indices_utils.py
+++ pybacmman/pandas/indices_utils.py
@@ -1,11 +1,15 @@
-
-
-getParent = lambda s : '-'.join(s.split('-')[:-1])
-def getPreviousFrame(currentIndices):
+# bacmman indices (barcode) manipulation
+getParent = lambda indices : '-'.join(indices.split('-')[:-1])
+getFrame = lambda indices : int(indices.split('-')[0])
+def getPrevious(currentIndices):
     spl = currentIndices.split('-')
     spl[0] = str(int(spl[0])-1)
     return '-'.join(spl)
-def getNextFrame(currentIndices):
+def getNext(currentIndices):
     spl = currentIndices.split('-')
     spl[0] = str(int(spl[0])+1)
+    return '-'.join(spl)
+def setFrame(indices, newFrame):
+    spl = indices.split('-')
+    spl[0] = str(newFrame)
     return '-'.join(spl)
