0b5f32be72643f3781c5f97e83aea29fd57cdf53
jeanollion <smogollion@yahoo.fr>
2025-03-24 15:37:12 +0100

    v0.6.3

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyBacmman",
-    version="0.6.2",
+    version="0.6.3",
     author="Jean Ollion",
     author_email="jean.ollion@polytechnique.org",
     description="Utilities for analysis of data generated from bacmman software",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jeanollion/PyBacmman.git",
-    download_url = 'https://github.com/jeanollion/PyBacmman/archive/v0.6.2.tar.gz',
+    download_url = 'https://github.com/jeanollion/PyBacmman/archive/v0.6.3.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
